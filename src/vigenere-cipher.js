508be792d11ebe47599df404dd8523063dc33f4f
Андрей Нестерук <anjeinesty@gmail.com>
2021-09-23 22:47:46 +0300

    feat: add encrypt decrypt task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(props = true) {
+    this.isInverted = props;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  formulaVigenere(a, b, action) {
+    let char = '';
+    if (action === 'encrypt') {
+      char = (a.charCodeAt(0) + b.toUpperCase().charCodeAt(0)) % 26 + 65;
+    } else {
+      char = 65 + ((a.charCodeAt(0) - 65) - (b.toUpperCase().charCodeAt(0) - 65) + 26) % 26;
+    }
+    return String.fromCharCode(char);
+  }
+  generator(text, key, action) {
+    let result = '';
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    let arrayMessage = text.toUpperCase().split('');
+    let idxForKey = 0;
+    for (let i = 0; i < arrayMessage.length; i++) {
+      if (arrayMessage[i].charCodeAt(0) < 65 || arrayMessage[i].charCodeAt(0) > 90) {
+        result += arrayMessage[i];
+        continue;
+      }
+      if (idxForKey === key.length) {
+        idxForKey = 0;
+      }
+      let encryptedChar = this.formulaVigenere(arrayMessage[i], key[idxForKey], action);
+      result += encryptedChar;
+      idxForKey++;
+    }
+    return result;
+  }
+  encrypt(message, key) {
+    return !this.isInverted ? this.generator(message, key, 'encrypt').split('').reverse().join('') : this.generator(message, key, 'encrypt');
+  }
+  decrypt(encryptedMessage, key) {
+    return !this.isInverted ? this.generator(encryptedMessage, key, 'decrypt').split('').reverse().join('') : this.generator(encryptedMessage, key, 'decrypt');
   }
 }
